e95a0b2307bdfc772fd9f489cf884daa0ec3b193
Andre Borralho <andrefilipe_one@hotmail.com>
2014-05-15 22:41:05 +0200

    titulo dos videos removido

--- festivall/platforms/ios/www/js/videos.js
+++ festivall/platforms/ios/www/js/videos.js
@@ -10,18 +10,19 @@ function queryShowVideosSuccess(tx, results){
 
         var videos = results.rows;
         var videos_length = videos.length;
+        $('#band_videos_scroller').empty();
 
-        if(videos_length >0 )
+        if(videos_length >0)
             for(var i = 0; i <videos_length; i++){
                 var video = videos.item(i);
                 var video_name = video.name;
                 var video_url = video.url;
                 var video_id = video.id;
 
-                $('#band_videos_scroller').empty().append(''+
+                $('#band_videos_scroller').append(''+
                         '<div id="video_' + video_id + '">' +
                         '<h3 class="video_name">' + video_name + '</h3>' +
-                        '<iframe width="'+video_url+'" height="200" src="http://www.youtube.com/embed/'+video_url+'" frameborder="0" allowfullscreen></iframe>'+
+                        '<iframe width="'+video_url+'" height="200" src="http://www.youtube.com/embed/'+video_url+'?showinfo=0" frameborder="0" allowfullscreen></iframe>'+
                         '</div>'
                 );
             }
