77596d5700cc45fc377205609747389a60da2e7a
Andre Borralho <andrefilipe_one@hotmail.com>
2014-05-16 00:50:50 +0200

    mais merdas do plugin

--- festivall/platforms/ios/www/cordova_plugins.js
+++ festivall/platforms/ios/www/cordova_plugins.js
@@ -28,13 +28,21 @@ module.exports = [
         "clobbers": [
             "Connection"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.console": "0.2.9-dev",
-    "org.apache.cordova.network-information": "0.2.8"
+    "org.apache.cordova.network-information": "0.2.8",
+    "org.apache.cordova.dialogs": "0.2.8-dev"
 }
 // BOTTOM OF METADATA
 });
